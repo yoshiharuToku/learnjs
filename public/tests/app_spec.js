c77a9492759fe16dfaa2f006e6fcd9950e23a8a9
ytoku003 <ytoku003@gmail.com>
2018-04-24 15:24:41 +0900

    update app_spec

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -1,4 +1,4 @@
-descrive('LearnJS', function() {
+describe('LearnJS', function() {
   it('can show a problem view', function() {
     learnjs.showView('#problem-1');
     expect($('.view-container .problem-view').length).toEqual(1);
