9b2fd991f87e8f0a6ab1c4ef2d25501542f713de
ytoku003 <ytoku003@gmail.com>
2018-04-26 11:35:57 +0900

    update index app appspec

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -1,6 +1,10 @@
-describe('LearnJS', function() {
+describse('LearnJS', function() {
   it('can show a problem view', function() {
     learnjs.showView('#problem-1');
     expect($('.view-container .problem-view').length).toEqual(1);
+  });
+  it('show the landing page view when there is no hash', function() {
+    learnjs.showView('');
+    except($('.view-container .landing-view').length).toEqual(1);
   });
 });
