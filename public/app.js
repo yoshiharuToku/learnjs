54ec784e4f82e80869a215005298ae329b8fa0d9
ytoku003 <ytoku003@gmail.com>
2018-04-26 12:21:02 +0900

    debug 2200

--- public/app.js
+++ public/app.js
@@ -3,16 +3,19 @@
 var learnjs = {};
 
 learnjs.problemView = function() {
-  return $('<div> class="problem-view"').text('coming soon!');
+  return $('<div class="problem-view">').text('coming soon!');
 }
 
 learnjs.showView = function(hash) {
+//  console.log(hash);
   var routes = {
-    'problem-1': learnjs.problemView
+    '#problem-1': learnjs.problemView
   }
   var viewFn = routes[hash];
+//  console.log(viewFn());
   if (viewFn) {
-    $('.view-container').empty().append(viewFn);
+//    console.log(viewFn());
+    $('.view-container').empty().append(viewFn());
   }
 //  var problemView = $('<div class="problem-view">').text('Coming soon!');
 //  $('.view-container').empty().append(problemView);
--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -1,10 +1,10 @@
-describse('LearnJS', function() {
+describe('LearnJS', function() {
   it('can show a problem view', function() {
     learnjs.showView('#problem-1');
     expect($('.view-container .problem-view').length).toEqual(1);
   });
   it('show the landing page view when there is no hash', function() {
     learnjs.showView('');
-    except($('.view-container .landing-view').length).toEqual(1);
+    expect($('.view-container .landing-view').length).toEqual(1);
   });
 });
