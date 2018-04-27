27457a0376a4b52b0ebeee5b131075b2ea8a72ce
ytoku003 <ytoku003@gmail.com>
2018-04-28 08:18:35 +0900

    update 3100-2

--- public/app.js
+++ public/app.js
@@ -31,13 +31,15 @@ learnjs.appOnReady = function() {
   learnjs.showView(window.location.hash);
 }
 
-learnjs.problems = {
+//source[learnjs/3100/public/app.js]{
+learnjs.problems = [
   {
-    description: "What is truth"
+    description: "What is truth",
     code: "function problem() { return __; }"
-  }
+  },
   {
-    description: "Simple Math"
+    description: "Simple Math",
     code: "function problem() { return 42 == 6 * __; }"
   }
-}
+];
+//}
