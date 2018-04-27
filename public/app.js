afa29463ef83f2279411f15866cc418bc971d103
ytoku003 <ytoku003@gmail.com>
2018-04-28 07:56:48 +0900

    update 3001

--- public/app.js
+++ public/app.js
@@ -3,8 +3,11 @@
 var learnjs = {};
 
 learnjs.problemView = function(problemNumber) {
-  var title = 'Problem #' + problemNumber + ' Coming soon!';
-  return $('<div class="problem-view">').text(title);
+  var view = $('.templates .problem-view').clone();
+  view.find('.title').text('Problem #' + problemNumber + ' Coming soon!');
+  return view;
+//  var title = 'Problem #' + problemNumber + ' Coming soon!';
+//  return $('<div class="problem-view">').text(title);
 }
 
 learnjs.showView = function(hash) {
